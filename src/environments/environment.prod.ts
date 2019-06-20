a19bde4c54fc8d7967396765f1a93e5eeb6c82d8
Fer Ayguavives <fer.aiwa@gmail.com>
2019-06-20 19:47:50 +0200

    Auto-generated commit

--- src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  apiBaseUrl: 'https://tururu-app.herokuapp.com',
-  cloudinaryBaseUrl: 'https://res.cloudinary.com/hackabos01fer/image/upload/',
-  cloudinaryBaseSound: 'https://res.cloudinary.com/hackabos01fer/video/upload/v1560899096/tururu-sounds/',
-};
